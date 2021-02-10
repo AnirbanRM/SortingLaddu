003e361a5d810b93c216ea1c643408772144f46d
Anirban <anirbanroymukherjee@outlook.com>
2021-02-11 04:52:17 +0530

    BubbleGum Sort

---
+++ src/BubbleGum.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class BubbleGum {
+	
+	public static void main(String[] param) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hey buddy! How many elements to sort ??? ");
+		
+		int n = s.nextInt();
+		
+		System.out.print("Enter your numbers one by one :) - ");
+		
+		int mylist[] = new int[5];
+		
+		for(int i = 0; i< n; i++)
+			mylist[i] = s.nextInt();
+		
+		bubbleSort(mylist);
+		
+		System.out.print("Your sorted list -> ");
+		for(int i = 0; i< n; i++) {
+			System.out.print(mylist[i]+" ");
+		}
+	}
+	
+	private static void bubbleSort(int[] alist) {
+		
+		int list_length = alist.length;
+		for(int i = 0; i< list_length; i++)
+			for(int j = 0; j< list_length-i-1; j++)
+				if(alist[j]>alist[j+1]) {
+					int t = alist[j];
+					alist[j]= alist[j+1];
+					alist[j+1] = t;
+				}
+	}
+
+}
